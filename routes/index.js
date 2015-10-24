6b6b5489f9b20c1cd7b2297ee80c18d5f496a214
KyCodeHuynh <KyCodeHuynh@gmail.com>
2015-10-24 14:07:07 -0700

    Move MySQL connection establishment/management to index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'db',
+  password : 'bruins111'
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
