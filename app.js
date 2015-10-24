ab05bb3b331690f6cfeff99bfd2e1d996ba2909c
Akshat Mahajan <akshatm.bkk@gmail.com>
2015-10-24 13:14:09 -0700

    Added database integration with Express; MySQL should be on your computer.

--- app.js
+++ app.js
@@ -4,11 +4,16 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var mysql      = require('mysql');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'bruin111'
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
