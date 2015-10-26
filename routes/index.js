42904adf4396b05e5bde9bd4c75782d67c1a87a1
Akshat Mahajan <akshatm.bkk@gmail.com>
2015-10-25 20:30:53 -0700

    Added DataTables

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,17 @@
 var express = require('express');
-var mysql  = require('mysql');
+var Handlebars = require('hbs');
+var mysql = require('mysql');
 var passport = require('passport');
 var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
 var router = express.Router();
 
 passport.serializeUser(function(user, done) {
-  done(null, user);
+    done(null, user);
 });
 
 passport.deserializeUser(function(obj, done) {
-  done(null, obj);
+    done(null, obj);
 });
 
 // UNCOMMENT THE FOLLOWING ONCE A CLIENT ID AND SECRET ARE OBTAINED
@@ -21,7 +22,7 @@ passport.deserializeUser(function(obj, done) {
 //    callbackURL: "http://127.0.0.1:3000/auth/google/callback"
 //  },
 //  function(accessToken, refreshToken, profile, done) {
-    // asynchronous verification, for effect...
+// asynchronous verification, for effect...
 //    process.nextTick(function () {
 //      return done(null, profile);
 //    });
@@ -33,10 +34,10 @@ router.use(passport.session());
 
 /* Set connection parameters */
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'db',
-  password : 'bruins111', // need a more secure method than hardcoding here
-  database : 'test'
+    host: 'localhost',
+    user: 'db',
+    password: 'bruins111', // need a more secure method than hardcoding here
+    database: 'test'
 });
 
 /* GET home page. */
@@ -45,8 +46,7 @@ router.get('/', function(req, res, next) {
     // This makes a connection to the database and populates our initial response.
     connection.query('SELECT * FROM sources', function(err, rows, fields) {
         if (err) throw err;
-        console.log(rows[0]);
-        res.render('index', {title: 'Express', response:rows[0]});
+        res.render('index', {title: 'Express', response: rows[0]});
     });
 
 });
