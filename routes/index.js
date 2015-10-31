1f6b2c36b5dce0ed7939428bee03f0ad40249aed
Akshat Mahajan <akshatm.bkk@gmail.com>
2015-10-30 17:31:14 -0700

    Switched to basic OAuth flow

--- routes/index.js
+++ routes/index.js
@@ -1,40 +1,14 @@
 var express = require('express');
+var session = require('express-session');
 var Handlebars = require('hbs');
 var mysql = require('mysql');
 var passport = require('passport');
+var url = require('url');
+var queryHandler = require('querystring');
+var https = require('https');
 var GoogleStrategy = require('passport-google-oauth2').Strategy;
-var cookieParser = require('cookie-parser');
 
 var router = express.Router();
-
-router.use(cookieParser());
-
-router.use(passport.initialize());
-router.use(passport.session());
-
-passport.serializeUser(function(user, done) {
-    return done(null, user);
-});
-
-passport.deserializeUser(function(obj, done) {
-    return done(null, obj);
-});
-
-passport.use(new GoogleStrategy({
-        // TODO: in production, hide the following somehow
-        clientID: "557716849061-upiujmrik7ah05fc92p0e4ki38a3fiu4.apps.googleusercontent.com",
-        clientSecret: "nJ10A33GBagyT94ntGG0XNFa",
-        callbackURL: "http://127.0.0.1:3000/auth/google/callback",
-        passReqToCallback: true
-    },
-    function(request, accessToken, refreshToken, profile, done) {
-        User.findOrCreate({
-            googleId: profile.id
-        }, function(err, user) {
-            return done(err, user);
-        });
-    }
-));
 
 /* Set connection parameters */
 var connection = mysql.createConnection({
@@ -49,10 +23,18 @@ router.get('/login', function(req, res) {
     res.render('login');
 });
 
-/* POST for login */
-router.post('/login', passport.authenticate('google', {
-    scope: 'openid profile email'
-}));
+router.get('/auth/google', function(req, res) {
+
+    var queryContents = {
+        response_type: 'code',
+        client_id: "557716849061-upiujmrik7ah05fc92p0e4ki38a3fiu4.apps.googleusercontent.com",
+        redirect_uri: 'http://127.0.0.1:3000/auth/google/callback/1',
+        scope: 'email'
+    };
+
+    res.redirect('https:accounts.google.com/o/oauth2/auth?' + queryHandler.stringify(queryContents));
+
+});
 
 /* GET sources if logged in; if not, send to login. */
 router.get('/', ensureAuthenticated, function(req, res, next) {
@@ -60,7 +42,59 @@ router.get('/', ensureAuthenticated, function(req, res, next) {
 });
 
 /* Called when authentication finishes */
-router.get('/auth/google/callback', function(req, res) {
+router.get('/auth/google/callback/1', function(req, res) {
+
+    var response = url.parse(req.url, true)['query'];
+
+    if (response.error) {
+
+        console.log('There was an authentication error!');
+        res.redirect('/login');
+
+    } else {
+
+        var queryContents = {
+            code: response.code,
+            client_id: "557716849061-upiujmrik7ah05fc92p0e4ki38a3fiu4.apps.googleusercontent.com",
+            client_secret: 'nJ10A33GBagyT94ntGG0XNFa',
+            redirect_uri: 'http://127.0.0.1:3000/auth/google/callback/2',
+            grant_type: 'authorization_code'
+        };
+        
+        console.log()
+
+        var requestParams = {
+            hostname: 'www.googleapis.com',
+            path: '/oauth2/v3/token',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Content-Length': queryHandler.stringify(queryContents).length
+            }
+        };
+
+        var request = https.request(requestParams, function(res) {
+            console.log('STATUS: ' + res.statusCode);
+            console.log('HEADERS: ' + JSON.stringify(res.headers));
+            res.setEncoding('utf8');
+            res.on('data', function(chunk) {
+                console.log('BODY: ' + chunk);
+            });
+            res.on('end', function() {
+                console.log('No more data in response.')
+            });
+        });
+
+        request.on('error', function(e) {
+            console.log('problem with request: ' + e.message);
+        });
+
+        request.write(queryHandler.stringify(queryContents));
+        request.end();
+    }
+});
+
+router.get('/auth/google/callback/2', function(req, res) {
     res.redirect('/sources');
 });
 
