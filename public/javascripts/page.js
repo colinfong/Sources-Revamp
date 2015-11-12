a497241dd17bdabe95dee22e604dc83d90ad78d2
Akshat Mahajan <akshatm.bkk@gmail.com>
2015-11-12 00:34:37 -0800

    Added ugly modal editor

--- public/javascripts/page.js
+++ public/javascripts/page.js
@@ -1,5 +1,22 @@
 // Bad JS follows! We should never have anything as a global variable.
-// Some refactoring would be advisable!
+// Some refactoring would be advisable! Maybe wrap as a closure in $(document).ready()? 
+
+Handlebars.registerHelper('map', function(text) {
+    var map = {
+        "name": 'Name',
+        "title": 'Title',
+        "organisation": "Organisation",
+        "work-phone": "Work Phone",
+        "cell-phone": "Cell Phone",
+        "other-phone": "Other Phone",
+        "work-email": "Work Email",
+        "personal-email": "Personal Email",
+        "notes": "Notes"
+    };
+    return map[text];
+});
+
+var template = Handlebars.compile($("#modal-template").html());
 
 var table = $('#sourceTable').DataTable({
     dom: '<"row"<"col-md-6"B><"col-md-6"f>>t<"row"<"col-md-6"i><"col-md-6"p>>',
@@ -8,46 +25,87 @@ var table = $('#sourceTable').DataTable({
     buttons: [{
         text: 'Edit',
         action: function(e, dt, node, config) {
-            // get the data!
+
+            // necessary variables
             var oldData = dt.row({
                 selected: true
-            }).data();
-            // alter the data!
-            var newData = $.extend({}, oldData); // create shallow copy
-            newData[0] = "ANDDNDDNDNDND";
-            // set the data!
-            $.ajax({
-                type: "POST",
-                contentType: "application/json",
-                url: "/edit",
-                data: JSON.stringify({
-                    old: oldData,
-                    new: newData
-                }),
-                success: function(event) {
-                    console.log("It worked");
+            }).data(); // current row data
+
+            var headers = ["name", "title", "organisation", "work-phone", "cell-phone", "other-phone", "work-email", "personal-email", "notes"]; // titles of columns
+
+            // render modal
+            $("#modal-container").html(template({
+                about: 'Edit',
+                header: headers
+            }));
+
+            // display modal
+            $("#myModal").modal();
+            
+            for (var i = 0; i < headers.length; i++) {
+                    $("#" + headers[i]).val(oldData[i]);
+            }
+
+            // custom event handlers on submit and close
+            $(".submit").on('click', function() {
+                $('#myModal').modal('hide');
+
+                var newData = [];
+
+                for (var i = 0; i < headers.length; i++) {
+                    var value = $("#" + headers[i]).val();
+                    console.log(value);
+                    if (value) {
+                        newData.push(value);
+                    } else {
+                        newData.push(oldData[i]);
+                    }
                 }
+
+                $.ajax({
+                    type: "POST",
+                    contentType: "application/json",
+                    url: "/edit",
+                    data: JSON.stringify({
+                        old: oldData,
+                        new: newData
+                    }),
+                    success: function(event) {
+                        console.log("It worked");
+                    }
+                });
+
+                dt.row({
+                    selected: true
+                }).data(newData).draw();
+                dt.columns.adjust().draw();
             });
-            dt.row({
-                selected: true
-            }).data(newData).draw();
+
+            $(".close").on('click', function() {
+                $('#myModal').modal('hide');
+            });
         },
         enabled: false
     }, {
         text: 'Delete',
         action: function(e, dt, node, config) {
-            // delete the data!
-            swal({
-                title: "Are you sure?",
-                text: "You are about to delete the selected row",
-                type: "warning",
-                showCancelButton: true,
-                confirmButtonText: "Yes, delete it!"
-            }, function() {
+
+            $("#modal-container").html(template({
+                about: 'Delete',
+                delete: true
+            }));
+
+            // display modal
+            $("#myModal").modal();
+
+            $(".submit").on('click', function() {
+
                 var row = dt.row({
                     selected: true
                 });
-                row.deselect();
+
+                $('#myModal').modal('hide');
+
                 $.ajax({
                     type: "POST",
                     contentType: "application/json",
@@ -57,31 +115,67 @@ var table = $('#sourceTable').DataTable({
                         console.log("It worked");
                     }
                 });
+
+                row.deselect();
+
                 row.remove().draw('full-hold');
+            });
+
+            $(".close").on('click', function() {
+                $('#myModal').modal('hide');
             });
         },
         enabled: false
     }, {
         text: 'Add',
         action: function(e, dt, node, config) {
-            // deselect selected rows
-            dt.row({
-                selected: true
-            }).deselect();
-            // add a row!
-            var newRowData = ["Name", "Title", "Organisation", "workPhone", "cellPhone", "otherPhone", "workEmail", "personalEmail", "notes"];
-            $.ajax({
-                type: "POST",
-                contentType: "application/json",
-                url: "/add",
-                data: JSON.stringify(newRowData),
-                success: function(event) {
-                    console.log("It worked");
+
+            var headers = ["name", "title", "organisation", "work-phone", "cell-phone", "other-phone", "work-email", "personal-email", "notes"]; // titles of columns
+
+            $("#modal-container").html(template({
+                about: 'Add',
+                header: headers
+            }));
+
+            // display modal
+            $("#myModal").modal();
+
+            $(".submit").on('click', function() {
+
+                var newData = [];
+                var allDataNull = true;
+
+                for (var i = 0; i < headers.length; i++) {
+                    var value = $("#" + headers[i]).val();
+                    newData.push(value);
+                    if (value) {
+                        allDataNull = false
+                    }
                 }
+
+                if (!allDataNull) {
+                    $.ajax({
+                        type: "POST",
+                        contentType: "application/json",
+                        url: "/add",
+                        data: JSON.stringify(newData),
+                        success: function(event) {
+                            console.log("It worked");
+                        }
+                    });
+
+                    dt.row.add(newData).draw(true);
+                    // jump to last page
+                    dt.page('last').draw('page');
+                    $('#myModal').modal('hide');
+                }
+
+                // do nothing otherwise
             });
-            dt.row.add(newRowData).draw(true);
-            // jump to last page
-            dt.page('last').draw('page');
+
+            $(".close").on('click', function() {
+                $('#myModal').modal('hide');
+            });
         },
         enabled: true
     }]
