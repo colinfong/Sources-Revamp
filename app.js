d2b5602f3333b9954c528b9f995a3b7e815c97a0
Nathan Smith <nathan@nathansmith.io>
2017-11-08 09:00:22 -0800

    prettier + eslint

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
-var express = require('express');
-var path = require('path');;
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require("express");
+const path = require("path");
+const logger = require("morgan");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
 
-var routes = require('./routes/index');
+const routes = require("./routes/index");
 
-var app = express();
+const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/', routes); // so all / and /* pages are handled here.
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/", routes); // so all / and /* pages are handled here.
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+app.use((req, res, next) => {
+  const err = new Error("Not Found");
   err.status = 404;
   next(err);
 });
@@ -30,10 +30,10 @@ app.use(function(req, res, next) {
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+if (app.get("env") === "development") {
+  app.use((err, req, res) => {
     res.status(err.status || 500);
-    res.render('error', {
+    res.render("error", {
       message: err.message,
       error: err
     });
@@ -42,9 +42,9 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use((err, req, res) => {
   res.status(err.status || 500);
-  res.render('error', {
+  res.render("error", {
     message: err.message,
     error: {}
   });
--- helpers/configAuth.js
+++ helpers/configAuth.js
@@ -5,18 +5,19 @@ and Google.
 
 In production: put this file under .gitignore! 
 
-*/ 
-var configs = {
-    database:{
-        host: process.env.MYSQL_HOST,
-        user: process.env.MYSQL_USER,
-        password: process.env.MYSQL_PWD,
-        database: process.env.MYSQL_DATABASE
-    },
-    oauth: {
-        client_id: process.env.G_CLIENT_ID,
-        client_secret: process.env.G_CLIENT_SECRET
-    }
+*/
+
+const configs = {
+  database: {
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PWD,
+    database: process.env.MYSQL_DATABASE
+  },
+  oauth: {
+    client_id: process.env.G_CLIENT_ID,
+    client_secret: process.env.G_CLIENT_SECRET
+  }
 };
 
 module.exports = configs;
