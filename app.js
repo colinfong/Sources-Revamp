ac6b783ff27f8e1e959ef03eb07b1e3a7c9ec0ad
Nathan Smith <nathan@nathansmith.io>
2017-11-08 13:48:09 -0800

    not done

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const nunjucks = require('nunjucks');
 
 const routes = require('./routes/index');
 
@@ -10,7 +11,10 @@ const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app,
+});
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
