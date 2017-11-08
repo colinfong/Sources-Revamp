0262eea14dbca99cfff20bc8e37844611bf5699b
Nathan Smith <nathan@nathansmith.io>
2017-11-08 11:44:57 -0800

    Squashed commit of the following:
    commit 3dc4d5be7636dca9955ef0fa736b75c6bc34db9b
    Author: Nathan Smith <nathan@nathansmith.io>
    Date:   Wed Nov 8 11:41:33 2017 -0800
    
        editorconfig (#4)
    
    commit 66212e8838c9727d0d6b66d27d5fb6bb364711a8
    Author: Nathan Smith <nathan@nathansmith.io>
    Date:   Wed Nov 8 11:05:27 2017 -0800
    
        .github folder (#3)
    
    commit ea0730675d2e072fb4e859988ce38166199b1100
    Merge: 3d62560 f43ca91
    Author: Nathan Smith <nathan@nathansmith.io>
    Date:   Wed Nov 8 10:59:22 2017 -0800
    
        Merge pull request #2 from daily-bruin/nathan/lint
    
        Lint
    
    commit f43ca9114173d016341d21d67a6adaa20b02237f
    Author: Nathan Smith <nathan@nathansmith.io>
    Date:   Wed Nov 8 10:49:58 2017 -0800
    
        blah
    
    commit d2b5602f3333b9954c528b9f995a3b7e815c97a0
    Author: Nathan Smith <nathan@nathansmith.io>
    Date:   Wed Nov 8 09:00:22 2017 -0800
    
        prettier + eslint

--- public/javascripts/page.js
+++ public/javascripts/page.js
@@ -1,211 +1,217 @@
 // This code operates on the main DataTables page generated
 // by index.hbs.
 
-;
 (function() {
+  Handlebars.registerHelper('map', function(text) {
+    // this registers a helper that maps different fields in the MySQL database to a simple name representation in the modal interface.
+    var map = {
+      name: 'Name',
+      organisation: 'Organization',
+      phones: 'Phones',
+      emails: 'Emails',
+      notes: 'Notes',
+    };
+    return map[text];
+  });
 
-    Handlebars.registerHelper('map', function(text) {
-        // this registers a helper that maps different fields in the MySQL database to a simple name representation in the modal interface. 
-        var map = {
-            "name": 'Name',
-            "organisation": "Organization",
-            "phones": "Phones",
-            "emails": "Emails",
-            "notes": "Notes"
-        };
-        return map[text];
-    });
+  //
+  var template = Handlebars.compile($('#modal-template').html());
 
-    // 
-    var template = Handlebars.compile($("#modal-template").html());
+  var table = $('#sourceTable').DataTable({
+    dom: '<"row"<"col-md-6"B><"col-md-6"f>>t<"row"<"col-md-6"i><"col-md-6"p>>',
+    select: true,
+    responsive: true,
+    buttons: [
+      {
+        text: 'Edit',
+        action: function(e, dt, node, config) {
+          // This function handles the Edit functionality
+          // when clicked.
 
-    var table = $('#sourceTable').DataTable({
-        dom: '<"row"<"col-md-6"B><"col-md-6"f>>t<"row"<"col-md-6"i><"col-md-6"p>>',
-        select: true,
-        responsive: true,
-        buttons: [{
-            text: 'Edit',
-            action: function(e, dt, node, config) {
-              
-                // This function handles the Edit functionality
-                // when clicked.
+          // necessary variables
+          var oldData = dt
+            .row({
+              selected: true,
+            })
+            .data(); // current row data
 
-                // necessary variables
-                var oldData = dt.row({
-                    selected: true
-                }).data(); // current row data
+          var headers = ['name', 'organisation', 'phones', 'emails', 'notes']; // titles of columns
 
-                var headers = ["name", "organisation", "phones", "emails", "notes"]; // titles of columns
+          // render modal
+          $('#modal-container').html(
+            template({
+              about: 'Edit',
+              header: headers,
+            })
+          );
 
-                // render modal
-                $("#modal-container").html(template({
-                    about: 'Edit',
-                    header: headers
-                }));
+          // display modal
+          $('#myModal').modal();
 
-                // display modal
-                $("#myModal").modal();
+          // populate modal fields
+          for (var i = 0; i < headers.length; i++) {
+            $('#' + headers[i]).val(oldData[i]);
+          }
 
-                // populate modal fields
-                for (var i = 0; i < headers.length; i++) {
-                    $("#" + headers[i]).val(oldData[i]);
-                }
+          // custom event handlers on submit and close
+          $('.submit').on('click', function() {
+            $('#myModal').modal('hide');
 
-                // custom event handlers on submit and close
-                $(".submit").on('click', function() {
-                    $('#myModal').modal('hide');
+            var newData = [];
 
-                    var newData = [];
+            for (var i = 0; i < headers.length; i++) {
+              var value = $('#' + headers[i]).val();
+              console.log(value);
+              if (value) {
+                newData.push(value);
+              } else {
+                newData.push(oldData[i]);
+              }
+            }
 
-                    for (var i = 0; i < headers.length; i++) {
-                        var value = $("#" + headers[i]).val();
-                        console.log(value);
-                        if (value) {
-                            newData.push(value);
-                        } else {
-                            newData.push(oldData[i]);
-                        }
-                    }
+            $.ajax({
+              type: 'POST',
+              contentType: 'application/json',
+              url: '/edit',
+              data: JSON.stringify({
+                old: oldData,
+                new: newData,
+              }),
+              success: function(event) {
+                console.log('Edited row');
+              },
+            });
 
-                    $.ajax({
-                        type: "POST",
-                        contentType: "application/json",
-                        url: "/edit",
-                        data: JSON.stringify({
-                            old: oldData,
-                            new: newData
-                        }),
-                        success: function(event) {
-                            console.log("Edited row");
-                        }
-                    });
+            dt
+              .row({
+                selected: true,
+              })
+              .data(newData)
+              .draw();
+          });
+        },
+        enabled: false,
+      },
+      {
+        text: 'Delete',
+        action: function(e, dt, node, config) {
+          // This function handles the Delete
+          // functionality when clicked.
 
-                    dt.row({
-                        selected: true
-                    }).data(newData).draw();
-                });
+          $('#modal-container').html(
+            template({
+              about: 'Delete',
+              delete: true,
+            })
+          );
 
-            },
-            enabled: false
-        }, {
-            text: 'Delete',
-            action: function(e, dt, node, config) {
-              
-                // This function handles the Delete 
-                // functionality when clicked.
+          // display modal
+          $('#myModal').modal();
 
-                $("#modal-container").html(template({
-                    about: 'Delete',
-                    delete: true
-                }));
+          // on submission, send AJAX request to handle
+          // deletion with data.
+          $('.submit').on('click', function() {
+            var row = dt.row({
+              selected: true,
+            });
 
-                // display modal
-                $("#myModal").modal();
+            $('#myModal').modal('hide');
 
-                // on submission, send AJAX request to handle
-                // deletion with data.
-                $(".submit").on('click', function() {
+            $.ajax({
+              type: 'POST',
+              contentType: 'application/json',
+              url: '/delete',
+              data: JSON.stringify(row.data()),
+              success: function(event) {
+                console.log('Deleted row');
+              },
+            });
 
-                    var row = dt.row({
-                        selected: true
-                    });
+            row.deselect();
 
-                    $('#myModal').modal('hide');
+            row.remove().draw('full-hold');
+          });
+        },
+        enabled: false,
+      },
+      {
+        text: 'Add',
+        action: function(e, dt, node, config) {
+          // This function handles the Add functionality
+          // when clicked.
 
-                    $.ajax({
-                        type: "POST",
-                        contentType: "application/json",
-                        url: "/delete",
-                        data: JSON.stringify(row.data()),
-                        success: function(event) {
-                            console.log("Deleted row");
-                        }
-                    });
+          var headers = ['name', 'organisation', 'phones', 'emails', 'notes']; // titles of columns
 
-                    row.deselect();
+          $('#modal-container').html(
+            template({
+              about: 'Add',
+              header: headers,
+            })
+          );
 
-                    row.remove().draw('full-hold');
-                });
+          // populate modal fields
+          for (var i = 0; i < headers.length; i++) {
+            $('#' + headers[i]).val('N/A');
+          }
 
-            },
-            enabled: false
-        }, {
-            text: 'Add',
-            action: function(e, dt, node, config) {
-              
-                // This function handles the Add functionality
-                // when clicked.
+          // display modal
+          $('#myModal').modal();
 
-                var headers = ["name", "organisation", "phones", "emails", "notes"]; // titles of columns
+          // on modal submission, check if every value is filled. If there is a missing value, refuse to proceed. If there isn't, send AJAX request to handle addition and redraw page.
+          $('.submit').on('click', function() {
+            var newData = [];
+            var allDataNull = true;
 
-                $("#modal-container").html(template({
-                    about: 'Add',
-                    header: headers
-                })); 
+            for (var i = 0; i < headers.length; i++) {
+              var value = $('#' + headers[i]).val();
+              newData.push(value);
+              if (value) {
+                allDataNull = false;
+              }
+            }
 
-                // populate modal fields
-                for (var i = 0; i < headers.length; i++) {
-                    $("#" + headers[i]).val("N/A");
-                }
+            if (!allDataNull) {
+              $.ajax({
+                type: 'POST',
+                contentType: 'application/json',
+                url: '/add',
+                data: JSON.stringify(newData),
+                success: function(event) {
+                  console.log('Added row');
+                },
+              });
 
-                // display modal
-                $("#myModal").modal();
-                
-                // on modal submission, check if every value is filled. If there is a missing value, refuse to proceed. If there isn't, send AJAX request to handle addition and redraw page.
-                $(".submit").on('click', function() {
+              // add a new row with the new data.
+              dt.row.add(newData).draw(true);
+              // jump to last page
+              dt.page('last').draw('page');
+              $('#myModal').modal('hide');
+            }
 
-                    var newData = [];
-                    var allDataNull = true;
+            // do nothing otherwise
+          });
+        },
+        enabled: true,
+      },
+    ],
+  });
 
-                    for (var i = 0; i < headers.length; i++) {
-                        var value = $("#" + headers[i]).val();
-                        newData.push(value);
-                        if (value) {
-                            allDataNull = false;
-                        }
-                    }
+  // handles selection
+  table.on('select', function() {
+    var selectedRows = table
+      .rows({
+        selected: true,
+      })
+      .count();
 
-                    if (!allDataNull) {
-                        $.ajax({
-                            type: "POST",
-                            contentType: "application/json",
-                            url: "/add",
-                            data: JSON.stringify(newData),
-                            success: function(event) {
-                                console.log("Added row");
-                            }
-                        });
-                      
-                        // add a new row with the new data.
-                        dt.row.add(newData).draw(true);
-                        // jump to last page
-                        dt.page('last').draw('page');
-                        $('#myModal').modal('hide');
-                    }
+    // enables edit and delete if exactly one row is selected.
+    table.button(0).enable(selectedRows == 1);
+    table.button(1).enable(selectedRows == 1);
+  });
 
-                    // do nothing otherwise
-                });
-
-            },
-            enabled: true
-        }]
-    });
-
-    // handles selection
-    table.on('select', function() {
-        var selectedRows = table.rows({
-            selected: true
-        }).count();
-
-        // enables edit and delete if exactly one row is selected.
-        table.button(0).enable(selectedRows == 1);
-        table.button(1).enable(selectedRows == 1);
-    });
-
-    // on deselection, disables edit and delete.
-    table.on('deselect', function() {
-        table.button(0).disable();
-        table.button(1).disable();
-    });
-
+  // on deselection, disables edit and delete.
+  table.on('deselect', function() {
+    table.button(0).disable();
+    table.button(1).disable();
+  });
 })();
--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,28 @@
-var express = require('express');
-var Handlebars = require('hbs');
-var google = require('googleapis');
-var url = require('url');
-var bodyParser = require('body-parser');
-var _ = require('lodash'); // generally useful library! 
+const express = require('express');
+const Handlebars = require('hbs');
+const google = require('googleapis');
+const url = require('url');
+const bodyParser = require('body-parser');
+const _ = require('lodash'); // generally useful library!
 
 // configuration settings
-var configAuth = require('../helpers/configAuth');
+const configAuth = require('../helpers/configAuth');
 
 // An optimised SQL query builder!
-var knex = require('knex')({
-    client: 'mysql',
-    connection: configAuth.database
+const knex = require('knex')({
+  client: 'mysql',
+  connection: configAuth.database,
 });
 
-var router = express.Router();
+const router = express.Router();
 // the following are needed to ensure JSON data sent through POST
 // requests are correctly decoded.
 router.use(bodyParser.json());
-router.use(bodyParser.urlencoded({
-    extended: false
-}));
+router.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
 
 /* 
 
@@ -42,59 +44,56 @@ ensureAuthenticated
 */
 
 // required libraries
-var OAuth2 = google.auth.OAuth2; // for pure authentication purposes
-var Profile = google.oauth2('v2'); // to obtain profile details
+const OAuth2 = google.auth.OAuth2; // for pure authentication purposes
+const Profile = google.oauth2('v2'); // to obtain profile details
 
-var oauth2Client = new OAuth2(configAuth.oauth.client_id,
-    configAuth.oauth.client_secret,
-    'http://sources.dailybruin.com/auth/google/callback'); // OAuth2 client to handle grunt work
+const oauth2Client = new OAuth2(
+  configAuth.oauth.client_id,
+  configAuth.oauth.client_secret,
+  'http://sources.dailybruin.com/auth/google/callback'
+); // OAuth2 client to handle grunt work
 
 // called when we want to begin authentication.
-router.get('/auth/google', function(req, res) {
+router.get('/auth/google', (req, res) => {
+  const url = oauth2Client.generateAuthUrl({
+    scope: 'email',
+  });
 
-    var url = oauth2Client.generateAuthUrl({
-        scope: 'email'
-    });
-
-    res.redirect(url);
-
+  res.redirect(url);
 });
 
 // called as next step in authentication process, redirected from URL in previous call
-router.get('/auth/google/callback', function(req, res) {
+router.get('/auth/google/callback', (req, res) => {
+  const response = url.parse(req.url, true).query; // get authentication code
 
-    var response = url.parse(req.url, true)['query']; // get authentication code
-
-    if (response.error) {
-
-        console.log(response.error); // print to terminal
-        res.redirect('/login'); // send user back to login to try again
-
-    } else {
-
-        oauth2Client.getToken(response.code, function(err, tokens) {
-            // Now tokens contains an access_token and an optional refresh_token. Save them.
-            if (!err) {
-                oauth2Client.setCredentials(tokens);
-                res.redirect('/sources'); // TODO: Make this asynchronous so as not to impact user experience!
-            }
-        });
-
-    }
+  if (response.error) {
+    console.log(response.error); // print to terminal
+    res.redirect('/login'); // send user back to login to try again
+  } else {
+    oauth2Client.getToken(response.code, (err, tokens) => {
+      // Now tokens contains an access_token and an optional refresh_token. Save them.
+      if (!err) {
+        oauth2Client.setCredentials(tokens);
+        res.redirect('/sources'); // TODO: Make this asynchronous so as not to impact user experience!
+      }
+    });
+  }
 });
 
 // Ensure that user is authenticated whenever they try to access a protected resource
 function ensureAuthenticated(req, res, next) {
-
-    Profile.userinfo.v2.me.get({
-        auth: oauth2Client
-    }, function(err, response) {
-        if (err || response.hd !== 'media.ucla.edu') {
-            res.redirect('/login');
-        } else {
-            return next();
-        }
-    });
+  Profile.userinfo.v2.me.get(
+    {
+      auth: oauth2Client,
+    },
+    (err, response) => {
+      if (err || response.hd !== 'media.ucla.edu') {
+        res.redirect('/login');
+      } else {
+        return next();
+      }
+    }
+  );
 }
 
 /* 
@@ -107,82 +106,80 @@ requests. It also includes post requests to update database properties.
 */
 
 // On accessing /sources, load sources from database
-router.get('/sources', ensureAuthenticated, function(req, res) {
-
-    knex.select().table('sources')
-        .then(
-            function(rows) {
-                res.render('index', {
-                    title: 'Daily Bruin Sources',
-                    response: rows
-                });
-            })
-        .catch(function(error) {
-            console.error(error);
-        });
-
+router.get('/sources', ensureAuthenticated, (req, res) => {
+  knex
+    .select()
+    .table('sources')
+    .then(rows => {
+      res.render('index', {
+        title: 'Daily Bruin Sources',
+        response: rows,
+      });
+    })
+    .catch(error => {
+      console.error(error);
+    });
 });
 
 /* on add, POST the data to MySQL */
 
-router.post('/add', ensureAuthenticated, function(req, res, next) {
-    var colNames = ['name', 'org', 'phones', 'emails', 'notes'];
+router.post('/add', ensureAuthenticated, (req, res) => {
+  const colNames = ['name', 'org', 'phones', 'emails', 'notes'];
 
-    var data = _.object(colNames, req.body);
+  const data = _.object(colNames, req.body);
 
-    knex('sources').insert(data)
-        .then(function(array) {
-            console.log(array);
-            res.status(204).end();
-        })
-        .catch(function(error) {
-            console.error(error);
-            res.status(204).end();
-        });
+  knex('sources')
+    .insert(data)
+    .then(array => {
+      console.log(array);
+      res.status(204).end();
+    })
+    .catch(error => {
+      console.error(error);
+      res.status(204).end();
+    });
 });
 
 // on edit, POST the data to MySQL.
 
-router.post('/edit', ensureAuthenticated, function(req, res, next) {
+router.post('/edit', ensureAuthenticated, (req, res) => {
+  const colNames = ['name', 'org', 'phones', 'emails', 'notes'];
 
-    var colNames = ['name', 'org', 'phones', 'emails', 'notes'];
+  const oldData = _.object(colNames, req.body.old);
+  const newData = _.object(colNames, req.body.new);
 
-    var oldData = _.object(colNames, req.body.old);
-    var newData = _.object(colNames, req.body.new);
-
-    knex('sources').where(oldData)
-        .update(newData)
-        .then(function(num) {
-            console.log('Updated ' + num + ' rows');
-            res.status(204).end();
-        })
-        .catch(function(error) {
-            console.error(error);
-            res.status(204).end();
-        });
+  knex('sources')
+    .where(oldData)
+    .update(newData)
+    .then(num => {
+      console.log(`Updated ${num} rows`);
+      res.status(204).end();
+    })
+    .catch(error => {
+      console.error(error);
+      res.status(204).end();
+    });
 });
 
 // on delete, POST the data to MySQL.
 
-router.post('/delete', ensureAuthenticated, function(req, res, next) {
+router.post('/delete', ensureAuthenticated, (req, res) => {
+  const colNames = ['name', 'org', 'phones', 'emails', 'notes'];
+  // Lodash creates an object by combining key array and value array
 
+  const data = _.object(colNames, req.body);
 
-    var colNames = ['name', 'org', 'phones', 'emails', 'notes'];
-    // Lodash creates an object by combining key array and value array
-
-    var data = _.object(colNames, req.body);
-
-    knex('sources')
-        .where(data)
-        .del()
-        .then(function(num) {
-            console.log('Deleted ' + num + ' rows');
-            res.status(204).end();
-        })
-        .catch(function(error) {
-            console.error(error);
-            res.status(204).end();
-        });
+  knex('sources')
+    .where(data)
+    .del()
+    .then(num => {
+      console.log(`Deleted ${num} rows`);
+      res.status(204).end();
+    })
+    .catch(error => {
+      console.error(error);
+      res.status(204).end();
+    });
 });
 
 // We don't use a method to update MySQL on clicking 'Add'.
@@ -200,14 +197,14 @@ Current routes:
 
 */
 
-// GET login page 
-router.get('/login', function(req, res) {
-    res.render('login');
+// GET login page
+router.get('/login', (req, res) => {
+  res.render('login');
 });
 
 // GET sources if logged in; if not, send to login.
-router.get('/', ensureAuthenticated, function(req, res, next) {
-    res.redirect('/sources');
+router.get('/', ensureAuthenticated, (req, res, next) => {
+  res.redirect('/sources');
 });
 
 module.exports = router;
