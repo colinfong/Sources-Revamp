9e20ac2cc92247824e93d67b8759808c32e09b14
Nathan Smith <nathan@nathansmith.io>
2017-11-08 13:39:39 -0800

    Squashed commit of the following:
    commit 3dc4d5be7636dca9955ef0fa736b75c6bc34db9b
    Author: Nathan Smith <nathan@nathansmith.io>
    Date:   Wed Nov 8 11:41:33 2017 -0800
    
        editorconfig (#4)
    
    commit 66212e8838c9727d0d6b66d27d5fb6bb364711a8
    Author: Nathan Smith <nathan@nathansmith.io>
    Date:   Wed Nov 8 11:05:27 2017 -0800
    
        .github folder (#3)
    
    commit ea0730675d2e072fb4e859988ce38166199b1100
    Merge: 3d62560 f43ca91
    Author: Nathan Smith <nathan@nathansmith.io>
    Date:   Wed Nov 8 10:59:22 2017 -0800
    
        Merge pull request #2 from daily-bruin/nathan/lint
    
        Lint
    
    commit f43ca9114173d016341d21d67a6adaa20b02237f
    Author: Nathan Smith <nathan@nathansmith.io>
    Date:   Wed Nov 8 10:49:58 2017 -0800
    
        blah
    
    commit d2b5602f3333b9954c528b9f995a3b7e815c97a0
    Author: Nathan Smith <nathan@nathansmith.io>
    Date:   Wed Nov 8 09:00:22 2017 -0800
    
        prettier + eslint

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-var express = require('express');
-var path = require('path');;
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
+const routes = require('./routes/index');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,8 +20,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes); // so all / and /* pages are handled here.
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
@@ -31,22 +31,22 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+  app.use((err, req, res) => {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
-      error: err
+      error: err,
     });
   });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use((err, req, res) => {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
-    error: {}
+    error: {},
   });
 });
 
